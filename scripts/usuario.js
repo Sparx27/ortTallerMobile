7798899e1b6fabb1620e5a592395cfd403aaad6a
Cristian <cri_1112@hotmail.com>
2024-07-31 19:28:44 -0300

    Prueba

--- scripts/usuario.js
+++ scripts/usuario.js
@@ -1,22 +1,23 @@
 let usuario = {
   userid: window.localStorage.getItem("iduser"),
-  apikey: window.localStorage.getItem("apikey")
-}
+  apikey: window.localStorage.getItem("apikey"),
+};
 
 function actualizarUsuario(iduser, apikey) {
-  window.localStorage.setItem("iduser", iduser)
-  window.localStorage.setItem("apikey", apikey)
-  usuario.userid = window.localStorage.getItem("iduser")
-  usuario.apikey = window.localStorage.getItem("apikey")
+  window.localStorage.setItem("iduser", iduser);
+  window.localStorage.setItem("apikey", apikey);
+  usuario.userid = window.localStorage.getItem("iduser");
+  usuario.apikey = window.localStorage.getItem("apikey");
 }
 
 function logout() {
-  window.localStorage.removeItem("iduser")
-  window.localStorage.removeItem("apikey")
+  window.localStorage.removeItem("iduser");
+  window.localStorage.removeItem("apikey");
 }
 
 function getUsuario() {
-  return usuario
+  return usuario;
 }
 
-export { getUsuario, actualizarUsuario, logout }
+export { getUsuario, actualizarUsuario, logout };
+//prueba
